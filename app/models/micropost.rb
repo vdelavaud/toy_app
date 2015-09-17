2892db54208bdff5eeae38e9cc820c3040716dd7
vdelavaud <virginie.delavaud@cornis.fr>
2015-09-17 14:47:31 +0200

    fin de toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,6 @@
 class Micropost < ActiveRecord::Base
+
+  belongs_to :user
+  # limiter la longueur d'un post
+  validates :content, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+  #un utilisateur peut avoir autant de posts qu'il veut
+  has_many :microposts
 end
