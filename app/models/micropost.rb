bafa78a12f39e6c60e32b5554ffe31e8fcb76782
vdelavaud <virginie.delavaud@cornis.fr>
2015-09-17 15:22:59 +0200

    exo 1 : test de la validation de contenu d'un psot

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ActiveRecord::Base
 
   belongs_to :user
+
   # limiter la longueur d'un post
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
